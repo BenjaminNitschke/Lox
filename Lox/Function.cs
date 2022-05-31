e782a9003959de04ffacc5c060f026d379131ca0
MuraliTandabany <muralitandabany@gmail.com>
2022-05-31 18:02:27 +0200

    Used File scoped namespace in Function.cs

--- Lox/Function.cs
+++ Lox/Function.cs
@@ -1,33 +1,32 @@
-﻿namespace Lox
+﻿namespace Lox;
+
+public class Function : Callable
 {
-	public class Function : Callable
+	public Function(Statement.FunctionStatement declaration, Environment closure)
 	{
-		public Function(Statement.FunctionStatement declaration, Environment closure)
-		{
-			this.declaration = declaration;
-			this.closure = closure;
-		}
-
-		private readonly Statement.FunctionStatement declaration;
-		private readonly Environment closure;
-		public int Arity() => declaration.functionParams.Count;
-
-		public object Call(Interpreter interpreter, List<object> arguments)
-		{
-			var environment = new Environment(closure);
-			for (var i = 0; i < declaration.functionParams.Count; i++)
-				environment.Define(declaration.functionParams[i].Lexeme, arguments[i]);
-			try
-			{
-				interpreter.ExecuteBlock(declaration.body, environment);
-			}
-			catch (Interpreter.Return returnValue)
-			{
-				return returnValue.value ?? new object();
-			}
-			return new object();
-		}
-
-		public override string ToString() => "<fn " + declaration.name.Lexeme + ">";
+		this.declaration = declaration;
+		this.closure = closure;
 	}
+
+	private readonly Statement.FunctionStatement declaration;
+	private readonly Environment closure;
+	public int Arity() => declaration.functionParams.Count;
+
+	public object Call(Interpreter interpreter, List<object> arguments)
+	{
+		var environment = new Environment(closure);
+		for (var i = 0; i < declaration.functionParams.Count; i++)
+			environment.Define(declaration.functionParams[i].Lexeme, arguments[i]);
+		try
+		{
+			interpreter.ExecuteBlock(declaration.body, environment);
+		}
+		catch (Interpreter.Return returnValue)
+		{
+			return returnValue.value ?? new object();
+		}
+		return new object();
+	}
+
+	public override string ToString() => "<fn " + declaration.name.Lexeme + ">";
 }
