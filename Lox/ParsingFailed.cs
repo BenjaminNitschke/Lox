686f038366f46bfce24f949c0ec271b03b2ba868
MuraliTandabany <muralitandabany@gmail.com>
2022-06-14 13:15:32 +0200

    Fixed File scoped namespace error

--- Lox/ParsingFailed.cs
+++ Lox/ParsingFailed.cs
@@ -1,48 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Lox;
 
-namespace Lox
+public class ParsingFailed : OperationFailed
 {
-	public class ParsingFailed : OperationFailed
+	protected ParsingFailed(Token token, string message = "") : base(message + " " + token.Type, token.Line) { }
+
+	public sealed class MissingSemicolon : ParsingFailed
 	{
-		protected ParsingFailed(Token token, string message = "") : base(message + " " + token.Type, token.Line) { }
+		public MissingSemicolon(Token token) : base(token) { }
+	}
 
-		public sealed class MissingSemicolon : ParsingFailed
-		{
-			public MissingSemicolon(Token token) : base(token) { }
-		}
+	public sealed class MissingClosingParenthesis : ParsingFailed
+	{
+		public MissingClosingParenthesis(Token token) : base(token) { }
+	}
 
-		public sealed class MissingClosingParenthesis : ParsingFailed
-		{
-			public MissingClosingParenthesis(Token token) : base(token) { }
-		}
+	public sealed class UnknownExpression : ParsingFailed
+	{
+		public UnknownExpression(Token token) : base(token) { }
+	}
 
-		public sealed class UnknownExpression : ParsingFailed
-		{
-			public UnknownExpression(Token token) : base(token) { }
-		}
+	public sealed class MissingVariableName : ParsingFailed
+	{
+		public MissingVariableName(Token token, string message = "") : base(token, message) { }
+	}
 
-		public sealed class MissingVariableName : ParsingFailed
-		{
-			public MissingVariableName(Token token, string message = "") : base(token, message) { }
-		}
+	public sealed class MissingRightBrace : ParsingFailed
+	{
+		public MissingRightBrace(Token token, string message = "") : base(token, message) { }
+	}
 
-		public sealed class MissingRightBrace : ParsingFailed
-		{
-			public MissingRightBrace(Token token, string message = "") : base(token, message) { }
-		}
+	public sealed class MissingLeftParenthesis : ParsingFailed
+	{
+		public MissingLeftParenthesis(Token token, string message = "") : base(token, message) { }
+	}
 
-		public sealed class MissingLeftParenthesis : ParsingFailed
-		{
-			public MissingLeftParenthesis(Token token, string message = "") : base(token, message) { }
-		}
-
-		public sealed class MissingLeftBrace : ParsingFailed
-		{
-			public MissingLeftBrace(Token token, string message = "") : base(token, message) { }
-		}
+	public sealed class MissingLeftBrace : ParsingFailed
+	{
+		public MissingLeftBrace(Token token, string message = "") : base(token, message) { }
 	}
 }
