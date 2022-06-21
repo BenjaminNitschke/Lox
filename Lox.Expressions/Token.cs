e4bebdf005b80bfb7ab6a8b079227c4c1674a643
MuraliTandabany <muralitandabany@gmail.com>
2022-06-21 17:38:09 +0200

    Changed Token from record to class type

--- Lox.Expressions/Token.cs
+++ Lox.Expressions/Token.cs
@@ -1,3 +1,17 @@
 ﻿namespace Expressions;
 
-public sealed record Token(TokenType Type, string Lexeme, object? Literal, int Line);
+public sealed class Token
+{
+	public TokenType Type { get; }
+	public string Lexeme { get; }
+	public object? Literal { get; }
+	public int Line { get; }
+
+	public Token(TokenType type, string lexeme, object? literal, int line)
+	{
+		Type = type;
+		Lexeme = lexeme;
+		Literal = literal;
+		Line = line;
+	}
+}
